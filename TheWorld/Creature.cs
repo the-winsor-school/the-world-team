b88015b334c33a06ce6588a1c06bcbe41df5092e
Karen Li <36568510+karenl03@users.noreply.github.com>
2020-04-14 20:40:14 -0400

    added inventory (not working)
    added a Inventory variable in the creatures class
    tried to implement a function that would drop all items from the creature's inventory when the creature was defeated but not sure how to put them into the CurrentArea.Items since Items is protected

--- TheWorld/Creature.cs
+++ TheWorld/Creature.cs
@@ -44,12 +44,21 @@ namespace TheWorld
 			set;
 		}
 
-        // TODO:  Moderate Achievement
-        // Give Creatures an Inventory (much like the Player's Backpack)
-        // When the Creature is defeated, these Items should then drop into the
-        // CurrentArea for the Player to be able to PickUp.
-        // (That logic will be added to the ProcessFightCommand method in the Win case.)
+		// TODO:  Moderate Achievement
+		// Give Creatures an Inventory (much like the Player's Backpack)
+		// When the Creature is defeated, these Items should then drop into the
+		// CurrentArea for the Player to be able to PickUp.
+		// (That logic will be added to the ProcessFightCommand method in the Win case.)
+		public Dictionary<string, List<ICarryableItem>> Inventory;
+        public void creatureDefeated(Creature c)
+        {
+            foreach (KeyValuePair<string, List<ICarryableItem>> item in c.Inventory)
+            {
+				TheGame.CurrentArea.Items.Add(item.Key, item.Value);
+                //not sure how to add item to current area bc Items is protected dictionary
+			}
 
-    }
+        }
+	}
 }
 
